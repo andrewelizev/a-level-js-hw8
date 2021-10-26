636477a7a10bc5d82fc1e468abbda35c21908982
Andrew Elizev PC <aelizev@gmail.com>
2021-10-27 01:18:00 +0300

    HW8 done

--- main.js
+++ main.js
@@ -40,26 +40,29 @@ function createButton(url) {
     return btn;
 }
 
+function linkToButton(str, cell) {
+    if (typeof str === 'string' && str.includes('https://swapi.dev/api/')) {
+        let btn = createButton(str);
+        cell.append(btn);
+        return cell;
+    } else {
+        cell.innerText = str;
+        return cell;
+    }
+}
+
 function createCell(cellData) {
     let cell = document.createElement('td');
 
-    if (typeof cellData === 'string' && cellData.includes('https://swapi.dev/api/')) {
-            let btn = createButton(cellData);
-            cell.append(btn);
-            return cell;
-        } else if (Array.isArray(cellData)) {
-
-            for (let elem of cellData) {
-                if (typeof elem === 'string' && elem.includes('https://swapi.dev/api/')) {
-                    let btn = createButton(elem);
-                    cell.append(btn);
-                }
-            }
-            return cell;
-        } else {
-            cell.innerText = cellData;
-            return cell;
+    if (Array.isArray(cellData)) {
+        for (let elem of cellData) {
+            linkToButton(elem, cell);
         }
+        return cell;
+    } else {
+        linkToButton(cellData, cell);
+        return cell;
+    }
 }
 
 function createRow(...cells) {
@@ -81,13 +84,13 @@ function jsonToTable(dom, data) {
         tableBody.append(row);
     }
 
-        table = document.getElementById('tableBody');
+    table = document.getElementById('tableBody');
 
-        if (!table) {
-            dom.append(tableBody);
-        } else {
-            table.replaceWith(tableBody);
-        }
+    if (!table) {
+        dom.append(tableBody);
+    } else {
+        table.replaceWith(tableBody);
+    }
 }
 
 
@@ -102,11 +105,55 @@ console.log('HW2 ==================================================');
 // функция отображения запускает сама себя(рекурсивно) для отображения новых данных в том же элементе.
 
 
+
 console.log('HW3 ==================================================');
 // HW3
+// myfetch
+// Используя XMLHTTPRequest, напишите промисифицированную функцию myfetch, т. е. функцию, которая возвращает промис,
+// и работает схоже с fetch, только в один этап:
+// myfetch('https://swapi.dev/api/people/1/')
+//   .then(luke => console.log(luke))
+// Функция myfetch ожидает что ответ будет в формате JSON (используйте JSON.parse(response.text))
+// В случае ошибок (request.onerror или request.status не 200) не забудьте вызывать reject
+// function myfetch(url){
+//     return new Promise(function (resolve, reject){
+//         const xhr = new XMLHTTPRequest()
+//         ///...
+//     })
+// }
 
+function myFetch(url) {
+    return new Promise(function (resolve){
+        let request = new XMLHttpRequest();
+
+        request.open('GET', url);
+        request.responseType = 'json';
+        request.send();
+
+        request.onload = function() {
+            if (request.status !== 200) {
+                return console.log(`${request.status}: ${request.statusText}`);
+            } else {
+                return resolve(request.response);
+            }
+        };
+    });
+}
+
+// myFetch('https://swapi.dev/api/people/1/')
+//   .then(luke => console.log(luke));
 
 
 
 console.log('HW4 ==================================================');
 // HW4
+// race
+// Используя Promise.race запустите запрос на API (myfetch) параллельно с delay. По результату определите,
+// что было быстрее, запрос по сети, или определенный интервал времени. Подберите параметр delay так,
+// что бы результат был неизвестен изначально, и при многократных запусках быстрее был то delay, то myfetch.
+const myDelay = new Promise((resolve, reject) => setTimeout(() => resolve(console.log('myDelay')), 75));
+
+const racePromis = Promise.race([
+    myFetch('https://swapi.dev/api/people/1/').then(luke => console.log('myFetch ', luke)),
+    myDelay
+]);
